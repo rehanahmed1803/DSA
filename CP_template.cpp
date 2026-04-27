b096fc0c6dea484f94b88db5657f844da1ac7890
Rehan Ahmed <somanirehan8@gmail.com>
2026-04-27 17:58:56 +0530

    Standard CP template

---
+++ CP_template.cpp
@@ -0,0 +1,27 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+#define int long long
+#define endl '\n'
+#define all(v) v.begin(), v.end()
+
+const int MOD = 1e9 + 7;  //most commonly used MOD in CP
+const int INF = 1e18;     //A "infinity" value that fits in long long — useful for min/max problems
+
+void solve() {
+  //solution goes here
+
+}
+
+signed main() {
+  ios_base::sync_with_stdio(false);
+  cin.tie(NULL);
+
+  int t = 1;
+  //cin >> t; If there are multiple tests involved
+  while(t--) {
+    solve();
+  }
+  return 0;
+}
+
